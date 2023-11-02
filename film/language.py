82f50b63273122b3b167e76279bd1de69bde59f4
GiseleLuque <agostinaacu28@gmail.com>
2023-11-02 11:29:48 -0300

    ACT6:agregue los actores de la pelicula

--- film/language.py
+++ film/language.py
@@ -1,7 +1,7 @@
 from flask import (Blueprint, flash, g, redirect, render_template, request, url_for)
 from werkzeug.exceptions import abort
 
-from flaskr.db import get_db
+from film.db import get_db
 
 bp = Blueprint('language', __name__, url_prefix="/language/")#nombra a todos languages al usar el prefix
 
